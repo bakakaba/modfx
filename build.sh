a64302f9c01d5c9177ce9ea92b487140b80a941d
Lian Hoy Lee <cruz@zyk3.net>
2017-02-09 15:43:09 +1300

    Clean out remaining artifacts from conversion.

--- build.sh
+++ build.sh
@@ -1,33 +1,16 @@
 #!/bin/bash
 
-function restore {
-	echo $'\n#### Restoring\n'
-	dotnet restore
-}
-
-function build {
-	echo $'\n#### Building src\n'
-	get_src_projects | xargs dotnet build
-
-	echo $'\n#### Building test\n'
-	get_test_projects | xargs dotnet build
-}
-
 function test {
-	echo $'\n#### Testing\n'
+	echo $'\n### Testing\n'
 	for project in $(get_test_projects); do
-		dotnet test $project -xml shippable/testresults/$(basename $project).xml
+		dotnet test $project
 	done
 }
 
-function get_src_projects {
-	find src -name 'project.json' -printf '%h\n'
-}
-
 function get_test_projects {
-	find test -name 'project.json' -printf '%h\n'
+	find test -name '*.csproj'
 }
 
-restore
-build
+dotnet restore
+dotnet build
 test
